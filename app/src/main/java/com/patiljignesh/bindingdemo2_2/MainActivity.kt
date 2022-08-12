bf61c377861d0f987ba8d8c3425dee4fbdfe77dd
Jignesh Patil <patiljignesh@gmail.com>
2022-08-12 16:52:27 +0200

    Initial commit

---
+++ app/src/main/java/com/patiljignesh/bindingdemo2_2/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.patiljignesh.bindingdemo2_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.patiljignesh.bindingdemo2_2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.apply {
+            helloWorldTextView.text = "New Demo!"
+            clickButton.setOnClickListener {
+                clickButton.text = "Cancel"
+            }
+        }
+    }
+}
